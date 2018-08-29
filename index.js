b9eeba63e14d21594788102c309637349b146af1
Dan Brimhall <dpbrimhall@hotmail.com>
2018-08-29 00:50:35 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,26 @@
+var recipes = { bread: 'wheat', cheese: 'cheddar'}
+
+
+function updateObjectWithKeyAndValue(object, key, value) {
+    var obj = { prop: 1 }
+    return Object.assign(obj, { prop2: 2} )
+}
+
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  var obj = { prop: 1 };
+  obj.prop2 = 2;
+  return obj
+}
+
+function deleteFromObjectByKey(object, key) {
+  var obj = { prop: 1 };
+  var newObj = Object.assign(obj)
+  delete newObj.prop;
+  return newObj
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  var obj = { prop: 1 };
+  delete obj.prop
+}

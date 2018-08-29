945cb86d8deeb7a9558ab3b02fdec55a14612435
Dan Brimhall <dpbrimhall@hotmail.com>
2018-08-29 01:06:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ function deleteFromObjectByKey(object, key) {
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  var obj = { prop: 1 };
-  return 
+  delete object.prop
+  return object
 }
